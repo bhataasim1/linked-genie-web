7352a89048b540468614a651d9594071a8c8d04d
bhataasim1 <bhataasim700@gmail.com>
2025-02-12 19:25:15 +0530

    ✨ Add hero card component with Chrome extension button and video dialog

---
+++ components/hero-card.tsx
@@ -0,0 +1,49 @@
+import { motion } from "framer-motion";
+import { containerVariants, itemVariants } from "@/lib/animation-variants";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { HeroVideoDialog } from "@/components/ui/hero-video-dialog";
+import { EnhancedButton } from "./ui/enhanced-btn";
+
+export const HeroCard = () => {
+  return (
+    <>
+      <motion.div
+        className="mt-6 flex w-full max-w-[24rem] justify-center gap-2"
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible">
+        <motion.div variants={itemVariants}>
+          <EnhancedButton
+            variant="expandIcon"
+            Icon={FaArrowRightLong}
+            onClick={() => { }}
+            iconPlacement="right"
+            className="mt-2 w-full"
+          >
+            Add to Chrome
+          </EnhancedButton>
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        className="relative mt-6 max-w-[70rem] flex justify-center"
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.div
+          variants={itemVariants}
+          className="w-full"
+        >
+          <HeroVideoDialog
+            className="block"
+            animationStyle="from-center"
+            videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+            thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
+            thumbnailAlt="Hero Video"
+          />
+        </motion.div>
+      </motion.div>
+    </>
+  )
+}
