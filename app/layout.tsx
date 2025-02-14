e8bd363ddea86149d9a5241d8242c4a7cbafd96e
bhataasim1 <bhataasim700@gmail.com>
2025-02-14 20:14:00 +0530

    ✨ Add OpenGraph and Twitter meta tags with new social sharing image

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      {/* <meta property="og:image" content="/opengraph-image.png" />
+      <meta property="og:image" content="/opengraph.png" />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1280" />
-      <meta property="og:image:height" content="832" /> */}
+      <meta property="og:image:height" content="832" />
       <meta
         property="og:site_name"
         content="Linked Genie"
@@ -29,10 +29,10 @@ export default function RootLayout({
         property="og:url"
         content="http://localhost:3000/"
       />
-      {/* <meta name="twitter:image" content="/twitter-image.png" />
+      <meta name="twitter:image" content="/opengraph.png" />
       <meta name="twitter:image:type" content="image/png" />
       <meta name="twitter:image:width" content="1280" />
-      <meta name="twitter:image:height" content="832" /> */}
+      <meta name="twitter:image:height" content="832" />
       <body className={FigtreeFont.className}>
         {children}
       </body>
